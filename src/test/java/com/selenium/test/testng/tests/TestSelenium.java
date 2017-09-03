87ab5c8cf191d0c4b652ab5a85d0d87e2c498328
Rob Bryan <rob@rmbryan.com>
2017-09-03 05:19:02 -0400

    Initial commit

---
+++ src/test/java/com/selenium/test/testng/tests/TestSelenium.java
@@ -0,0 +1,25 @@
+package com.selenium.test.testng.tests;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxBinary;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxProfile;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.io.File;
+
+public class TestSelenium {
+    public static void main(String[] args){
+
+        System.setProperty("webdriver.gecko.driver", "/usr/local/bin/geckodriver");
+        DesiredCapabilities capabilities = DesiredCapabilities.firefox();
+        capabilities.setCapability("marionette", true);
+
+        WebDriver driver = new FirefoxDriver(capabilities);
+
+        driver.get("http://www.findlectures.com");
+    }
+}
+
